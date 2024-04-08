0842f5459013d97178112e760bc86705576b4f26
mh-lamine <mlamins.ngom@gmail.com>
2024-04-08 13:59:29 +0200

    Fix issue with setCurrentArtist not being called when hasFeated is true

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,9 @@ export default function App() {
           currentArtist.toLowerCase()
       )
     );
-    setCurrentArtist(answer);
+    //FIXME: 
+    console.log(hasFeated)
+    hasFeated && setCurrentArtist(answer);
     setLoading(false);
   };
 
